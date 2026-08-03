66d0694f2be2834dcf013201daa01c3821ef08d0
thanhtrung <nguyenthanhtrungtl2006@gmail.com>
2026-08-03 22:41:43 +0700

    add Spring Boot Backend

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TvTmp3
+Backend java 21 maven 
+☑ Spring Web
+☑ Spring Data JPA
+☑ MySQL Driver
+☑ Lombok
+☑ Validation
+☑ Spring Boot DevTools
---
+++ backend/tvtmp3/src/test/java/com/tvt/tvtmp3/Tvtmp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvt.tvtmp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tvtmp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
